d4a832927d0266cb302787f9637e79994b93aaf3
Evgenii Pchelinov <epchelinov@gmail.com>
2025-03-31 15:08:27 +0500

    refactor: move API key file reading to reusable function

--- lua/custom/plugins/gp.lua
+++ lua/custom/plugins/gp.lua
@@ -1,10 +1,4 @@
-local config_path = vim.fn.stdpath 'config' .. '/.openai_api_key'
-local handle = io.popen('cat ' .. config_path)
-local api_key = handle:read '*a'
-handle:close()
-
-api_key = api_key:gsub('%s+$', '') -- Удаление пробелов в конце
-api_key = api_key:gsub('\n', '') -- Удаление символов новой строки
+local api_key = require('custom.utils.getApiKeyFromFile').getApiKeyFromFile 'openai'
 
 return {
   {
